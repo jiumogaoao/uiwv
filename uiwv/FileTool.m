00e9127c98af89e0591058158df0b5bc5752bc1b
jiumogaoao <394127821@qq.com>
2017-01-24 14:48:27 +0800

    change
    Change

--- uiwv/FileTool.m
+++ uiwv/FileTool.m
@@ -9,6 +9,23 @@
 #import "FileTool.h"
 
 @implementation FileTool
--(void)get:(NSString *)path target:(id)targetObject callback:(NSString *)callbackName{};
--(void)set:(NSString *)path data:(NSString *)dataStr target:(id)targetObject callback:(NSString *)callbackName{};
+-(void)get:(NSString *)path target:(id)targetObject callback:(NSString *)callbackName{
+    NSFileManager *fm=[NSFileManager defaultManager];
+    if([fm fileExistsAtPath:path]){
+        NSData *fileData = [[NSData alloc] initWithContentsOfFile:path];
+        SEL func_selector = NSSelectorFromString([NSString stringWithFormat:@"%@:",callbackName]);
+        if ([targetObject respondsToSelector:func_selector]) {
+            [targetObject performSelector:func_selector withObject:fileData];
+        }
+    }
+};
+-(void)set:(NSString *)path data:(NSData *)dataObject target:(id)targetObject callback:(NSString *)callbackName{
+    NSFileManager *fm=[NSFileManager defaultManager];
+    [fm removeItemAtPath:path error:nil];
+    [fm createFileAtPath:path contents:dataObject attributes:nil];
+    SEL func_selector = NSSelectorFromString([NSString stringWithFormat:@"%@:",callbackName]);
+    NSString *fileData=@"";
+    if ([targetObject respondsToSelector:func_selector]) {
+        [targetObject performSelector:func_selector withObject:fileData];
+    }};
 @end
